d0afddeb5f8721cebf678a71c59d18a08496410e
sharvaibhav <vaibhav.jiitu@gmail.com>
2018-06-26 02:23:20 +0530

    styling changes

--- src/components/Reporting.js
+++ src/components/Reporting.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-
+import './reporting.css'
 class Reporting extends Component {
     render(){
         return(
             <div className='reporting'>
+                <h2> Please press the button to download the Report </h2>
                 <a href="/api/healthReport" target="_blank">
                     <button className="download-report-button">Download Report</button>
                 </a>
