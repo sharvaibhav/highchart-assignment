58d4560cd3a8489efe1213ae8cc453dab60f8a3f
Ayush Walia <ayush.walia@flipkart.com>
2018-06-26 01:50:46 +0530

    Dummy Commit

--- src/components/Reporting.js
+++ src/components/Reporting.js
@@ -2,9 +2,13 @@ import React, {Component} from 'react';
 
 class Reporting extends Component {
     render(){
-        return(<div className='reporting'>
-                Placeholder for Reporting Component
-            </div>)
+        return(
+            <div className='reporting'>
+                <a href="/api/healthReport" target="_blank">
+                    <button className="download-report-button">Download Report</button>
+                </a>
+            </div>
+        );
     }
 }
 
--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import { Route } from 'react-router-dom'
-import {  } from "./Leftnavigation";
 import Dashboard from "./Dashboard";
 import Reporting from "./Reporting";
 import LeftNavigation from "./Leftnavigation";
@@ -9,11 +8,10 @@ export default class Main extends Component {
         return (
             <div className='container-fluid'>
                 <LeftNavigation />
-            <main className='col-md-9'>
-                <Route exact path="/" component={Dashboard} />
-                <Route exact path="/dashboard" component={Dashboard} />
-                <Route exact path="/reporting" component={Reporting} />
-            </main>
+                <main className='col-md-9'>
+                    <Route exact path="/dashboard" component={Dashboard} />
+                    <Route exact path="/reporting" component={Reporting} />
+                </main>
             </div>
         );
     }
